e9a81c811030c57f547d884f49e5046aa2b4b710
Eric Internicola <intere@gmail.com>
2017-02-18 17:41:28 -0700

    I can now move a white square around the maze.  Yay!

--- Shared/GameBoard.swift
+++ Shared/GameBoard.swift
@@ -8,13 +8,20 @@
 
 import SpriteKit
 
+// MARK: Board Type
+
 enum BoardType: Int {
+    case unknown = -1
     case wall = 0
     case open = 1
-    case unknown = -1
+    case playerStart = 2
 
+    /// Gets you a BoardType from an Integer value
+    ///
+    /// - Parameter value: The value you want converted to a board type
+    /// - Returns: The BoardType if it could be inferred, or `.unknown` if not
     static func from(value: Int) -> BoardType {
-        let types: [BoardType] = [.wall, .open]
+        let types: [BoardType] = [.wall, .open, .playerStart]
         for type in types {
             if type.rawValue == value {
                 return type
@@ -24,18 +31,27 @@ enum BoardType: Int {
     }
 }
 
+struct BoardPoint {
+    var x: Int = 0
+    var y: Int = 0
+}
+
+// MARK: - Game Board
+
 class GameBoard {
 
     /// 0 = wall, 1 = open
     var board: [[Int]]
-    var container: SKSpriteNode?
+    var container: SKSpriteNode!
     var scene: SKScene
+    var player: SKSpriteNode!
+    var playerLocation = BoardPoint()
 
     /// Default initializer
     init(inScene scene: SKScene) {
         board = [
             [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
-            [ 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0 ],
+            [ 0, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0 ],
             [ 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0 ],
             [ 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0 ],
             [ 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 0, 0, 1, 0 ],
@@ -49,7 +65,6 @@ class GameBoard {
             [ 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0 ],
             [ 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0 ],
             [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
-
         ]
         self.scene = scene
         drawBoard(in: scene)
@@ -61,41 +76,116 @@ class GameBoard {
 
 extension GameBoard {
 
+    func movePlayer(direction: Direction) {
+        let newPosition = move(direction: direction, from: playerLocation)
+        guard canMove(to: newPosition) else {
+            print("Illegal move: \(direction)")
+            return
+        }
+        move(node: player, to: newPosition, duration: 0.3)
+        playerLocation = newPosition
+    }
+
+    /// Maps you from a given point to a new point using the direction and point you provide
+    ///
+    /// - Parameters:
+    ///   - direction: The direction you want to move
+    ///   - point: The current location that you want to move from
+    /// - Returns: A point that represents where you want to move to
+    func move(direction: Direction, from point: BoardPoint) -> BoardPoint {
+        var x = point.x
+        var y = point.y
+
+        switch direction {
+        case .up:
+            y -= 1
+        case .down:
+            y += 1
+        case .left:
+            x -= 1
+        case .right:
+            x += 1
+        default:
+            break
+        }
+        return BoardPoint(x: x, y: y)
+    }
+
+    /// Tells you if a node can move to the given position
+    ///
+    /// - Parameter point: The board point you wish to move to
+    /// - Returns: True if you can move there, false otherwise
+    func canMove(to point: BoardPoint) -> Bool {
+        guard point.x >= 0, point.y >= 0, point.x < board.count, point.y < board[0].count else {
+            return false
+        }
+
+        switch BoardType.from(value: board[point.y][point.x]) {
+        case .open, .playerStart:
+            return true
+        default:
+            return false
+        }
+    }
+
+    /// Moves the provided node to the provided location on the board and takes the specified duration to do it
+    ///
+    /// - Parameters:
+    ///   - node: The node to be moved
+    ///   - point: The board location to move it to
+    ///   - duration: how long to take to animate the motion
+    func move(node: SKNode, to point: BoardPoint, duration: TimeInterval) {
+        let position = convert(boardPoint: point)
+        node.run(SKAction.move(to: position, duration: duration))
+    }
 }
 
 // MARK: - Helpers
 
 fileprivate extension GameBoard {
 
+    var boardSize: CGFloat {
+        return min(scene.size.width, scene.size.height) - 10   // buffered by at least 5px on all sides
+    }
+    var count: CGFloat {
+        return CGFloat(board.count)
+    }
+    var unitSize: CGFloat {
+        return boardSize / count
+    }
+
+    func convert(boardPoint: BoardPoint) -> CGPoint {
+        let x = position(index: boardPoint.x, row: false)
+        let y = position(index: boardPoint.y)
+
+        return CGPoint(x: x, y: y)
+    }
+
     /// Assumes the number of rows and columns is the same (a square)
     func drawBoard(in scene: SKScene) {
-        let boardSize = min(scene.size.width, scene.size.height) - 100   // buffered by at least 25px on all sides
-        let count = CGFloat(board.count)
-        let unitSize = boardSize/count
-
-        let container = SKSpriteNode(color: .clear, size: CGSize(width: boardSize, height: boardSize))
+        container = SKSpriteNode(color: .clear, size: CGSize(width: boardSize, height: boardSize))
         print("Scene Info: \(scene.frame)")
+        guard let container = container else {
+            assertionFailure("We just created the container but it doesn't exist, something fishy is going on")
+            return
+        }
         container.position = CGPoint(x: scene.frame.midX, y: scene.frame.midY)
         print("Container anchor: \(container.anchorPoint)")
         print("Container position: \(container.position)")
 
         for row in 0..<Int(count) {
-            let y = position(container: container, unitSize: unitSize, index: row, count: count)
             for column in 0..<Int(count) {
-                let x = position(container: container, unitSize: unitSize, index: column, count: count, row: false)
-                guard let node = draw(a: board[row][column], atX: x, andY: y, unitSize: unitSize) else {
+                guard let node = draw(a: board[row][column], at: BoardPoint(x: column, y: row)) else {
                     continue
                 }
                 container.addChild(node)
             }
         }
 
-        self.container = container
         container.physicsBody = SKPhysicsBody(rectangleOf: container.size)
         container.physicsBody?.affectedByGravity = false
         scene.addChild(container)
     }
-
 
     /// Figures out where to position the object in the specified container at the specified index
     ///
@@ -105,7 +195,7 @@ fileprivate extension GameBoard {
     ///   - index: The index of the unit to be placed
     ///   - count: The total number of units in the row
     /// - Returns: The value for placement
-    func position(container: SKNode, unitSize: CGFloat, index: Int, count: CGFloat, row: Bool = true) -> CGFloat {
+    func position(index: Int, row: Bool = true) -> CGFloat {
         let halfCount = count / 2
         let offset = abs(halfCount - CGFloat(index))
 
@@ -124,13 +214,28 @@ fileprivate extension GameBoard {
         }
     }
 
-    func draw(a type: Int, atX x: CGFloat, andY y: CGFloat, unitSize: CGFloat) -> SKSpriteNode? {
+    func draw(a type: Int, at boardPoint: BoardPoint) -> SKSpriteNode? {
+        let location = convert(boardPoint: boardPoint)
+
         switch BoardType.from(value: type) {
+        case .playerStart:
+            if let _ = self.player {
+                assertionFailure("Only 1 player per board")
+                return nil
+            }
+            let node = SKSpriteNode(color: .white, size: CGSize(width: unitSize, height: unitSize))
+            node.position = location
+            node.anchorPoint = CGPoint(x: 0, y: 0)
+            playerLocation = boardPoint
+            player = node
+            return node
+
         case .wall:
-            let node = SKSpriteNode(color: .green, size: CGSize(width: unitSize, height: unitSize))
-            node.position = CGPoint(x: x, y: y)
+            let node = SKSpriteNode(color: .blue, size: CGSize(width: unitSize, height: unitSize))
+            node.position = location
             node.anchorPoint = CGPoint(x: 0, y: 0)
             return node
+
         default:
             return nil
         }
